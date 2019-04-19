f6fe8dc9d8c3674834fd1ce5bef38cd42c6f302c
Mychal Hall <hallmychal@gmail.com>
2019-04-18 21:55:09 -0500

    I do not think I like redux

--- friends/src/index.js
+++ friends/src/index.js
@@ -1,12 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from 'react-router-dom';
+import "./index.css";
+import App from "./App";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import friendsReducer from "./reducers/friendsReducer";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(
+    friendsReducer, 
+    applyMiddleware(thunk));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(
+    <Router>
+  <Provider store={store}>
+    <App />
+  </Provider>
+  </Router>,
+  document.getElementById("root")
+);
+
---
+++ friends/src/reducers/friendsReducer.js
@@ -0,0 +1,12 @@
+
+const initialState = {
+    friends: []
+}
+
+const friendsReducer = (state = initialState, action) => {
+    switch(action.type) {
+        default:
+        return state;
+    }
+}
+export default friendsReducer;
